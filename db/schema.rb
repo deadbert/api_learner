9ae2c7704123f12a34e0510fcb19fcf7744b1bb4
Nick Spencer <strangebrew202@gmail.com>
2023-11-16 22:17:52 -0700

    Add: added user table with bcrypt hashing for password digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_17_041059) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_17_050557) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,13 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_17_041059) do
 
   create_table "nouns", force: :cascade do |t|
     t.string "noun"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
